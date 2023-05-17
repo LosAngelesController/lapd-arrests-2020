cfb153da8287ef6ef32711d026cf7df1aea9e137
na0nie <naonchun@gmail.com>
2023-05-17 09:56:25 -0700

    links for other years

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,15 +1,24 @@
 import ActiveLink from "./ActiveLink";
 const navigationPayroll = [
   {
-    name: "Map",
-    url: "/",
+    name: "2019",
+    url: "https://lapd-arrests-2019.vercel.app/",
   },
-  /*
   {
-    name: "Charts",
-    url: "/charts",
+    name: "2020",
+    url: "/",
     newtab: true,
-  },*/
+  },
+  {
+    name: "2021",
+    url: "https://lapd-arrests-2021.vercel.app/",
+    newtab: true,
+  },
+  {
+    name: "2022",
+    url: "https://lapd-arrests-2022.vercel.app/",
+    newtab: true,
+  },
   {
     name: "Los Angeles Controller",
     url: "https://controller.lacity.gov",
